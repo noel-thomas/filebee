71c95f1b7d7f1f25d010b138476a648a32ec6df0
Noel Thomas <official.noelthomas@gmail.com>
2022-11-02 07:49:56 +0530

    api basic url implementation for add and ls

--- app.py
+++ app.py
@@ -1,6 +1,32 @@
-from flask import Flask
-app = Flask(__name__)
+import os
+from flask import Flask, request, url_for
+from werkzeug.utils import secure_filename
+from markupsafe import escape
 
+UPLOAD_FOLDER = '/tmp'
+ALLOWED_EXTENSIONS = {'txt'}
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+# reply for root
 @app.route('/')
 def index():
     return 'Hello!'
+
+# response to api /add - to upload files
+@app.post('/add')
+def add_files():
+    f = request.files['the_file'] # the_file identification of uploaded file
+    f.save(f"/tmp/{secure_filename(f.filename)}")
+    return {"msg": "added"}
+
+# response for the api ls - to list the files
+@app.route('/ls')
+def list_files():
+    return f"{request(files)}"
+
+# sample test for custom route
+@app.route('/<string:datas>')
+def list_print(datas):
+    return f"Hi, {escape(datas)}"
